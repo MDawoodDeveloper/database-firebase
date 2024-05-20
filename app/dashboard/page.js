1a87ee8a42a3b26de42f8c52e7caaf4d9428a236
MDawoodDeveloper <145212464+MDawoodDeveloper@users.noreply.github.com>
2024-05-20 10:23:00 +0500

    Initial commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Navbar/>
+      Dashoard
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import Navbar from "./components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+    <h1>
+      Homepage
+    </h1>
+    </>
+  );
+}
